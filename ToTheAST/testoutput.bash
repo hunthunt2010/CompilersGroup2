57a973dc47451078726a106aac8dcd30417dfd8a
Matt Buland <gik0geck0@gmail.com>
2014-04-25 15:59:21 -0600

    Add add the test script and keith's test files

---
+++ ToTheAST/testoutput.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+parser="python3 $PWD/scanner.py"
+graphvis="python2 $PWD/parse-tree-to-graphvis.py"
+dot="dot -Tpng -o "
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: $0 testdir outdir"
+    exit 1
+fi
+
+if [ ! -d "$2" ]
+then mkdir "$2"; fi
+
+for file in $(ls $1)
+do
+    absfile="$PWD/$1/$file"
+    if [ ! -d "$2/$file" ]; then mkdir "$2/$file"; fi
+    pushd "$2/$file"
+        # echo "Parsing $absfile"
+        # echo "$parser < $absfile"
+        $parser < $absfile
+        cat OUTPUT.p | $graphvis | $dot parsetree.png
+        cat OUTPUT.a | $graphvis | $dot arithmetictree.png
+        # echo "Done parsing"
+    popd
+    echo ""
+done
