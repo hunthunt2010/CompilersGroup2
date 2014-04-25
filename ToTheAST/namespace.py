572cf559d6fe5b9b98f2257c8418547ec38c6c76
Taylor Schmidt <taylor@ubuntu.ubuntu-domain>
2014-04-24 20:37:27 -0600

    fixed the sloppy code I wrote this morning for namespace

--- ToTheAST/namespace.py
+++ ToTheAST/namespace.py
@@ -6,30 +6,30 @@
 
 class Namespace:
 
-	nameString = ''
-
 	def __init__ (self):
-		#Not much to initialize
+		self.nameString = ''
 
-	def addName (newName):
+	def addName(self, newName):
 		"takes a new name string as an arg and returns a tuple with its index and length in the global string"
 		
 		nameLength = len(newName)
 
 		#check if name already exists in namespace, if not add it
-		if nameString.find(newName) != -1:
-			nameIndex = nameString.find(newName)
+		if self.nameString.find(newName) != -1:
+			nameIndex = self.nameString.find(newName)
 			return (nameIndex, nameLength)
-		else
-			nameIndex = len(nameString)
-			nameString = nameString + newName
+		else:
+			nameIndex = len(self.nameString)
+			self.nameString = self.nameString + newName
 			return (nameIndex, nameLength)
 
-	def getName (nameTuple):
+	def getName(self, nameTuple):
 		"takes a name tuple as an arg (nameIndex, nameLength) and returns the string representation from the namespace"
 
 		nameIndex = nameTuple[0]
-		endIndex = nameIndex + (nameTuple[1] -1)
+		endIndex = nameIndex + (nameTuple[1])
 
-		return nameString[nameIndex:endIndex]
+		return self.nameString[nameIndex:endIndex]
+
+
 
