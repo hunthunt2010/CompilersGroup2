f765991a1177466ba3773ce2263ce28dcc628571
Taylor Jeffery Schmidt <taschmid@bb136-10.mines.edu>
2014-05-02 11:49:18 -0600

    Finished register class hopefully

--- ToTheAST/register.py
+++ ToTheAST/register.py
@@ -1,31 +1,60 @@
 #!/usr/bin/env python
 
+class Register:
+	def __init__(self, name, memory=None):
+		self.name = name
+		self.memoryloc = memory 
+		self.assigned = False
+
+
 class RegisterTracker:
 	def __init__(self):
 		self.allocRegNum = 9 
 		self.workRegNum = 3
 
+		self.allocRegList = []
 		#register representation
-		self.regList = [False] * self.allocRegNum
-		self.workRegList = [False] * self.workRegNum
+		for i in range(0, self.allocRegNum):
+			self.allocRegList.append(Register('R%d' % i))
+
+		self.workRegList = []
+		#register representation
+		for i in range(0, self.workRegNum):
+			self.workRegList.append(Register('R%d' % (i + self.allocRegNum)))
 
 		#stack pointer
 		self.sp = 34767
 
 	def getWorkReg(self):
-		for i in range(0, self.workRegNum - 1):
-			if self.workRegList[i] == False:
-				self.workRegList[i] = True
-				return i + self.allocRegNum
+		for i in range(0, self.workRegNum):
+			if self.workRegList[i].assigned == False:
+				self.workRegList[i].assigned = True
+				return workRegList[i]
 
 	def getReg(self):
-		for i in range(0, self.allocRegNum - 1):
-			if self.regList[i] == False:
-				self.regList[i] = True
-				return i
+		for i in range(0, self.allocRegNum):
+			if self.regList[i].assigned == False:
+				self.regList[i].assigned = True
+				return self.regList[i]
 
-	def freeReg(self, regNum):
-		self.regList[regNum] == False
+		#if Virtual
+		vReg = Register('Virtual', self.sp)
+		self.sp -= 8
+		return vReg
+
+	def freeAllocReg(self, regName):
+		for reg in self.allocRegList:
+			if reg.name == regName:
+				reg.assigned == False
+
+	def freeWorkReg(self, regName):
+		for reg in self.WorkRegList:
+			if reg.name == regName:
+				reg.assigned == False
+
+	#Passes in number of registers to free NOT amount of memory to free
+	def freeVirtualReg(self, numToFree):
+		self.sp += (numToFree * 8)
 
 
 
