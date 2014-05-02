e138ca6821fef8cb792eba5c44da34fdbd6897ae
Maria Deslis <mdeslis@mines.edu>
2014-05-02 15:09:57 -0600

    fixed deallocateVar and  lookUpVar, symboltable -> self.mmap, and fixed instructionList.append

--- ToTheAST/visitor.py
+++ ToTheAST/visitor.py
@@ -118,7 +118,6 @@ class ArithmeticTransformer(Visitor):
 
 class IntermediateRepresentation(Visitor):
         def __init__(self, symboltable, mmap, file=stdout):
-                self.table = SymbolTable()
                 self.output = file
                 self.mmap = mmap
                 self.symboltable = symboltable
@@ -180,33 +179,33 @@ class IntermediateRepresentation(Visitor):
                     i = node.children[0].data
                     j = self.symboltable.retrieveScope(i, stack=node.children[0].scopestack)
                     #print("memst RX,",self.mmap[j], file=self.output)
-                    instructionList.append("memst RX, %s" % str(self.mmap[j]))
+                    instructionList.append("memst RX, %s" % str(self.mmap.lookUpVar(node.children[0].data, node.children[0].scopestack)))
 
                 elif node.name == 'DECL':
                     currVarScope = self.symboltable.retrieveScope(node.children[1].data, stack=node.children[1].scopestack)
-                    symboltable.allocateScope([currVarScope])
+                    self.mmap.allocateScope([currVarScope])
 
                     if len(node.children) > 2:
                         if node.children[2].name != 'MULTI_ASSIGN':
                             #print("calc RX,",node.children[2], file=self.output)
                             instructionList.append("calc RX, %s" % str(node.children[2]))
-                            i = node.children[1].data
-                            j = self.symboltable.retrieveScope(i, stack=node.children[1].scopestack)
+                            # i = node.children[1].data
+                            # j = self.symboltable.retrieveScope(i, stack=node.children[1].scopestack)
                             #print("memst RX,",self.mmap[j], file=self.output)
-                            instructionList.append("memst RX, %s" % str(self.mmap[j]))
+                            instructionList.append("memst RX, %s" % str(self.mmap.lookUpVar(node.children[1].data, node.children[1].scopestack)))
                         else:
                             instructionList += self.visit(node.children[2])
 
                 elif node.name == 'MULTI_ASSIGN':
                     currVarScope = self.symboltable.retrieveScope(node.children[0].data, stack=node.children[0].scopestack)
-                    symboltable.allocateScope([currVarScope])
+                    self.mmap.allocateScope([currVarScope])
 
                     #print("calc RD,",node.children[1], file=self.output)
                     instructionList.append("calc RD, %s" % str(node.children[1]))
                     i = node.children[0].data
                     j = self.symboltable.retrieveScope(i)
                     #print("memst RX,",self.mmap[j], file=self.output)
-                    instructionList.append("memst RX, %s" % str(self.mmap[j]))
+                    instructionList.append("memst RX, %s" % str(self.mmap.lookUpVar(node.children[0].data, node.children[0].scopestack)))
                     if len(node.children) > 2 and node.children[2].name == 'MULTI_ASSIGN':
                         instructionList += self.visit(node.children[2])
 
@@ -223,7 +222,7 @@ class IntermediateRepresentation(Visitor):
                         instructionList += self.visit(child)
 
                     codeBlockScope = node.children[0].scopestack[0]
-                    symboltable.deallocateScope(codeBlockScope);
+                    self.mmap.deallocateScope(codeBlockScope);
 
                 elif node.name == 'START':
                     instructionList = []
